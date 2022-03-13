2f86f1f95d6c4427a185bcb71b2b3b35e52bafba
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-03-13 11:09:58 +0100

    Register user added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
+import { auth } from './config/firebaseConfig'
 
 function App() {
   const [registerEmail, setRegisterEmail] = useState('')
   const [registerPassword, setRegisterPassword] = useState('')
   const [loginEmail, setLoginEmail] = useState('')
   const [loginPassword, setLoginPassword] = useState('')
+  const [user, setUser] = useState({})
+
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser)
+  })
+
+  const register = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+      console.log(user)
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+  console.log(auth)
   return (
-    <></>
+    <>
+      <form>
+        <input type="text" onChange={(e) => { setRegisterEmail(e.target.value) }} />
+        <input type="text" onChange={(e) => { setRegisterPassword(e.target.value) }} />
+        <button onClick={register}>create user</button>
+      </form>
+      {user ? user.email : null}
+    </>
   )
 }
 
