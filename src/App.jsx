74276f9f7cd3e11294f4864bb34e08d28bcc778a
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-03-13 09:14:27 +0100

    Add config firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [registerEmail, setRegisterEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+  const [loginEmail, setLoginEmail] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  return (
+    <></>
+  )
+}
+
+export default App;
---
+++ src/utilities/auth.jsx
@@ -0,0 +1,5 @@
+const registerUser = async () => {
+
+}
+
+export { registerUser }
